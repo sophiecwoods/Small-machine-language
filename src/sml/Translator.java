792ed5ec2e65d6b7204329cc1f71cc91e04eeb91
Sophie Woods <sophiecwoods1@gmail.com>
2022-03-05 14:32:29 +0000

    Add switch case for DivInstruction

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -1,9 +1,6 @@
 package sml;
 
-import sml.instructions.AddInstruction;
-import sml.instructions.LinInstruction;
-import sml.instructions.MulInstruction;
-import sml.instructions.SubInstruction;
+import sml.instructions.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -107,6 +104,12 @@ public final class Translator {
                 s1 = scanInt();
                 s2 = scanInt();
                 return new MulInstruction(label, r, s1, s2);
+            }
+            case "div" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new DivInstruction(label, r, s1, s2);
             }
             case "lin" -> {
                 r = scanInt();
