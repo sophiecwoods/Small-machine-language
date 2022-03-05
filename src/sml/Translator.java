e056685ea19bd3b5b36a98cd53b2b304c761ba00
Sophie Woods <sophiecwoods1@gmail.com>
2022-03-05 12:32:49 +0000

    Add SubInstruction and MulInstruction cases

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -2,6 +2,8 @@ package sml;
 
 import sml.instructions.AddInstruction;
 import sml.instructions.LinInstruction;
+import sml.instructions.MulInstruction;
+import sml.instructions.SubInstruction;
 
 import java.io.File;
 import java.io.IOException;
@@ -93,6 +95,18 @@ public final class Translator {
                 s1 = scanInt();
                 s2 = scanInt();
                 return new AddInstruction(label, r, s1, s2);
+            }
+            case "sub" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new SubInstruction(label, r, s1, s2);
+            }
+            case "mul" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new MulInstruction(label, r, s1, s2);
             }
             case "lin" -> {
                 r = scanInt();
