092bda80d7531e1a89f5cff4c30b560f5034008d
Sophie Woods <sophiecwoods1@gmail.com>
2022-03-05 12:57:00 +0000

    Create outline DivInstruction class

---
+++ src/sml/instructions/DivInstruction.java
@@ -0,0 +1,32 @@
+package sml.instructions;
+
+import sml.Instruction;
+import sml.Machine;
+
+/**
+ * This class represents the Div (divide) instruction from the language.
+ *
+ * @author Sophie Woods
+ */
+public class DivInstruction extends Instruction {
+
+    private String label;
+    private int register;
+    private int s1;
+    private int s2;
+
+    public DivInstruction(String label, int register, int s1, int s2){
+        super(label, "div");
+        this.register = register;
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    @Override
+    public void execute(Machine m) {
+
+    }
+
+    @Override
+    public String toString() { return null; }
+}
