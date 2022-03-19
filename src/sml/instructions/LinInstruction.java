45820d9ead16f3a4822fd40479fc95b63eb682a2
Sophie Woods <sophiecwoods1@gmail.com>
2022-03-19 10:52:23 +0000

    Add mutator method setLinInstruction and add default constructor which calls mutator

--- src/sml/instructions/LinInstruction.java
+++ src/sml/instructions/LinInstruction.java
@@ -2,31 +2,58 @@ package sml.instructions;
 
 import sml.Instruction;
 import sml.Machine;
+import sml.Translator;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static sml.Translator.getTranslatorInst;
 
 /**
  * This class represents the Lin (store) instruction from the language.
  *
  * @author Sophie Woods
  */
+
 public class LinInstruction extends Instruction {
 
-    private String label;
     private int register;
-    private int x;
+    private int s1;
 
-    public LinInstruction(String label, int register, int x){
+    /*
+     * Default constructor
+     */
+    public LinInstruction(String label, String fileName, String opcode) throws InvocationTargetException,
+            NoSuchMethodException, IllegalAccessException {
+        super(label, opcode);
+        if (opcode.equals("lin")) setLinInstruction(fileName);
+    }
+
+    public LinInstruction(String label, int register, int s1){
         super(label, "lin");
         this.register = register;
-        this.x = x;
+        this.s1 = s1;
+    }
+
+    /*
+     * Mutator method to set the field values
+     */
+    public void setLinInstruction(String fileName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Translator translator = getTranslatorInst(fileName);
+        // use reflection to access Translator scanInt method
+        Method scanInt = Translator.class.getDeclaredMethod("scanInt");
+        scanInt.setAccessible(true);
+        this.register = (int) scanInt.invoke(translator);
+        this.s1 = (int) scanInt.invoke(translator);
     }
 
     @Override
     public void execute(Machine m) {
-        m.getRegisters().setRegister(register, x);
+        m.getRegisters().setRegister(register, s1);
     }
 
     @Override
     public String toString() {
-        return super.toString() + " " + register + " " + x;
+        return super.toString() + " " + register + " " + s1;
     }
 }
