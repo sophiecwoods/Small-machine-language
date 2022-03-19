a7145f4a5903bc15b511846c5711ea380053521c
Sophie Woods <sophiecwoods1@gmail.com>
2022-03-19 11:14:17 +0000

    Add mutator method setOutInstruction and add default constructor which calls mutator

--- src/sml/instructions/OutInstruction.java
+++ src/sml/instructions/OutInstruction.java
@@ -2,31 +2,56 @@ package sml.instructions;
 
 import sml.Instruction;
 import sml.Machine;
+import sml.Translator;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static sml.Translator.getTranslatorInst;
 
 /**
  * This class represents the Out (print) instruction from the language.
  *
  * @author Sophie Woods
  */
+
 public class OutInstruction extends Instruction {
 
-    private String label;
-    private int s1;
+    private int register;
 
+    /*
+     * Default constructor
+     */
+    public OutInstruction(String label, String fileName, String opcode) throws InvocationTargetException,
+            NoSuchMethodException, IllegalAccessException {
+        super(label,opcode);
+        if (opcode.equals("out")) setOutInstruction(fileName);
+    }
 
-    public OutInstruction(String label, int s1){
+    public OutInstruction(String label, int register){
         super(label, "out");
-        this.s1 = s1;
+        this.register = register;
 
+    }
+
+    /*
+     * Mutator method to set the field values
+     */
+    public void setOutInstruction(String fileName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Translator translator = getTranslatorInst(fileName);
+        // use reflection to access Translator scanInt method
+        Method scanInt = Translator.class.getDeclaredMethod("scanInt");
+        scanInt.setAccessible(true);
+        this.register = (int) scanInt.invoke(translator);
     }
 
     @Override
     public void execute(Machine m) {
-        System.out.println(m.getRegisters().getRegister(s1));
+        System.out.println(m.getRegisters().getRegister(register));
     }
 
     @Override
     public String toString() {
-        return super.toString() + " " + s1;
+        return super.toString() + " " + register;
     }
 }
