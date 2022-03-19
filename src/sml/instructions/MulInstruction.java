36b0fd3a3a6f55514ed9234f69c7df6cc95dcd9f
Sophie Woods <sophiecwoods1@gmail.com>
2022-03-19 11:11:59 +0000

    Add mutator method setMulInstruction and add default constructor which calls mutator

--- src/sml/instructions/MulInstruction.java
+++ src/sml/instructions/MulInstruction.java
@@ -2,24 +2,52 @@ package sml.instructions;
 
 import sml.Instruction;
 import sml.Machine;
+import sml.Translator;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static sml.Translator.getTranslatorInst;
 
 /**
  * This class represents the Mul (multiply) instruction from the language.
  *
  * @author Sophie Woods
  */
+
 public class MulInstruction extends Instruction {
 
-    private String label;
     private int register;
     private int s1;
     private int s2;
+
+    /*
+     * Default constructor
+     */
+    public MulInstruction(String label, String fileName, String opcode) throws InvocationTargetException,
+            NoSuchMethodException, IllegalAccessException {
+        super(label,opcode);
+        if (opcode.equals("mul")) setMulInstruction(fileName);
+    }
 
     public MulInstruction(String label, int register, int s1, int s2){
         super(label, "mul");
         this.register = register;
         this.s1 = s1;
         this.s2 = s2;
+    }
+
+    /*
+     * Mutator method to set the field values
+     */
+    public void setMulInstruction(String fileName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Translator translator = getTranslatorInst(fileName);
+        // use reflection to access Translator scanInt method
+        Method scanInt = Translator.class.getDeclaredMethod("scanInt");
+        scanInt.setAccessible(true);
+        this.register = (int) scanInt.invoke(translator);
+        this.s1 = (int) scanInt.invoke(translator);
+        this.s2 = (int) scanInt.invoke(translator);
     }
 
     @Override
